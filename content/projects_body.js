ba223b64abbb624abd5b37bb55e6d0d837cc59df
Ryboster <23017998@uhi.ac.uk>
2024-08-12 07:52:06 +0100

    patch

--- content/projects_body.js
+++ content/projects_body.js
@@ -3,23 +3,49 @@ function RenderBody() {
                 return(
                     <div style={{position: "relative"}}>
                         <div class="introContainer">
-                            <table>
-                                <a href="https://google.com/">
+                            <table class="projectsTable">
+                                
+                                    <a href="https://google.com/">
                                     <tr>
+                                        
                                         <td class="table_title">Scattered Lands </td>
-                                        <td class="table_description">Video Game (in progress) </td>
+                                        <td class="table_description"> Isometric RPG video game (In progress) </td>
                                         <td class="table_picture"><img src="../assets/scattered_lands.png" /></td>
+                                    
                                     </tr>
-                                </a>
-                                <a href="https://github.com/Ryboster/Active-Recall-Trainer">
+                                    </a>
+
+                                    <a href="https://github.com/Ryboster/Active-Recall-Trainer">
                                     <tr>
+                                        
                                         <td class="table_title">Active Recall Trainer </td>
-                                        <td class="table_description">Software designed to train active recall </td>
+                                        
+                                        <td class="table_description"> Solution for quicker recall of words </td>
                                         <td class="table_picture"><img src="../assets/active_recall_trainer.png" /></td>
+                                    </tr>          
+                                    </a>
+
+                                    <a href="https://github.com/Ryboster/WizLights-Remote-Python"> 
+                                    <tr>
+                                        <td class="table_title"> Infrared controlled LED </td>
+                                        <td class="table_description"> smart lights made more convenient with infrared.</td>
+                                        <td class="table_picture"><img src="../assets/active_recall_trainer.png" /></td>
+
                                     </tr>
-                                </a>               
+                                    </a>
+
+                                    <a href="https://github.com/Ryboster/reaction_role_discord_selfbot"> 
+                                    <tr>
+                                        <td class="table_title"> Moderation bot for Discord </td>
+                                        <td class="table_description"> Moderation made simpler with Python </td>
+                                        <td class="table_picture"><img src="../assets/active_recall_trainer.png" /></td>
+
+                                    </tr>
+                                    </a>
                             </table>       
                         </div>
                     </div>
                 )
             }
+
+
