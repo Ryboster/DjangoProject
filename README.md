2e9a08b47136980f5484acf85326cf2f45f11257
Ryboster <23017998@uhi.ac.uk>
2024-08-13 02:03:46 +0100

    updated main page

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ My name is Gracjan and this is my personal website. It is written in javascript
 ## Resources
 https://developer.mozilla.org/en-US/docs/Web/CSS/Comments
 https://developer.mozilla.org/en-US/docs/Web/CSS/Viewport_concepts
-
+https://www.w3schools.com/tags/tag_hr.asp
+https://forum.freecodecamp.org/t/making-a-whole-div-have-a-dynamic-href-link-react/340805
+https://stackoverflow.com/questions/13194692/align-h1-header-and-normal-text-in-same-line
+https://stackoverflow.com/questions/5421990/css-standard-dynamic-way-to-centralize-an-element-in-the-y-axis
+https://developer.mozilla.org/en-US/docs/Web/CSS/display
--- content/index_body.js
+++ content/index_body.js
@@ -1,26 +1,57 @@
 function RenderBody() {
                 
                 return(
-                    <div style={{position: "relative"}}>
-                        <div class="introContainer"> 
-                            <h1>
-                                Gracjan Blazejowski
-                            </h1>
-                            <h3>
-                               <i> Software Developer </i>
-                            </h3>
-                            <p style={{textAlign:"center"}}>
-                                Currently involved in collaborative development of a video game as a leader of a small team. Tenacious and determined
-                                IT professional with experience in several programming languages. Skilled in object oriented programming, version control
-                                systems and continuous integration. First hand experience in project management, SCRUM framework and Agile methodology.
-                                Embedded programming enthusiast. Future backend engineer.
-                            </p>
+                    <div>
+                        <div class="introContainer">
+                            <div className="Header">
+                                <h1> Gracjan Blazejowski </h1>
+                                <h2><i> Software Developer </i></h2>
+                                
+                            </div>
+                            <div className="subHeader">
+                                <h3>Overview:</h3>
+                                <hr />
+                            </div>
+
+                            <div className="overviewSection">
+                                <p>
+                                    Currently involved in collaborative development of a video game as a leader of a small team. Tenacious and determined
+                                    IT professional with experience in several programming languages. Skilled in object oriented programming, version control
+                                    systems and continuous integration. First hand experience in project management, SCRUM framework and Agile methodology.
+                                    Embedded programming enthusiast. Future backend engineer.
+                                </p>
+                            </div>
                         </div>
+
                         <div class="photoContainer">
-                            <img src="../assets/handsome_man.png" className="self_photo" /> <br />
-                            <img src="../assets/icons/li.svg" className="icons"/><a href="https://www.linkedin.com/in/gracjan-blazejowski-97b935233/">Gracjan Blazejowski</a> <br />
-                            <img src="../assets/icons/gh.svg" className="icons"/><a href="https://github.com/Ryboster">AmazonPython</a> <br />
-                            <img src="../assets/icons/em.png" className="icons"/><a href="mailto:gracjanblazejowski1@gmail.com">gracjanblazejowski1@gmail.com</a> <br />
+                            <div class="handsomeManDivider">
+                                <img src="../assets/handsome_man.png" className="self_photo" /> <br />
+                            </div>
+                            <div class="socials">
+                                
+                                <a href="https://www.linkedin.com/in/gracjan-blazejowski-97b935233/">
+                                    <div>
+                                        <img src="../assets/icons/li.svg" />
+                                        <p> Gracjan Blazejowski </p><br />
+                                    </div>
+                                </a>
+
+                                <a href="https://github.com/Ryboster">
+                                    <div>
+                                        <img src="../assets/icons/gh.svg" />
+                                        <p> AmazonPython </p> <br />
+                                    </div>
+                                </a>
+
+                                <a href="mailto:gracjanblazejowski1@gmail.com">
+                                    <div>
+                                        <img src="../assets/icons/em.png" />
+                                        <p> gracjanblazejowski1@gmail.com </p>
+                                    </div>
+                                </a> <br />
+                                   
+                                
+                            </div>
                         </div>
                     </div>
                 )
