a09d7b54c4808c9e33d6994e26d9fc9e31e43722
23017998 <23017998@uhi.ac.uk>
2024-12-31 23:29:21 +0000

    added the base for the CRUD

--- server/CRUD.py
+++ server/CRUD.py
@@ -22,29 +22,33 @@ class CRUD:
         self.CreateDatabase()
         self.InitializeDatabase()
 
+
+    def CreateDatabase(self):
+        ''' Make sure the database file exists '''
+        homeDatabase = os.path.join(SCRIPT_DIR, DB_NAME + DB_EXTENSION)
+        open(homeDatabase, 'a').close()
+
     def InitializeDatabase(self):
+        ''' Initialize tables inside the database '''
         connection = sqlite3.connect(DB_NAME + DB_EXTENSION)
         cursor = connection.cursor()
         cursor.execute(DB_INIT_COMMAND)
 
 
-    def CreateDatabase(self):
-        homeDatabase = os.path.join(SCRIPT_DIR, DB_NAME + DB_EXTENSION)
-        open(homeDatabase, 'a').close()
-
-
-    
-
-    def Create(self):
+    def Create(self, table, column, newValue):
+        ''' Insert new record into the database '''
         pass
     
-    def Read(self):
+    def Read(self, table, column):
+        ''' Retrieve existing record from the database '''
         pass
     
-    def Update(self):
+    def Update(self, table, column, newValue):
+        ''' Overwrite an existing record in the database '''
         pass
 
     def Delete(self):
+        ''' Delete a record from the database '''
         pass
 
     
