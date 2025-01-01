c561e23ce0dec76d57af6aa8230ed3cf25bd30e5
23017998 <23017998@uhi.ac.uk>
2025-01-01 04:14:53 +0000

    added comments to crud

--- server/CRUD.py
+++ server/CRUD.py
@@ -5,6 +5,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DB_NAME = "db"
 DB_EXTENSION = ".sqlite3"
+DB = DB_NAME + DB_EXTENSION
 
 
 DB_INIT_COMMAND = """
@@ -30,26 +31,55 @@ class CRUD:
 
     def InitializeDatabase(self):
         ''' Initialize tables inside the database '''
-        connection = sqlite3.connect(DB_NAME + DB_EXTENSION)
-        cursor = connection.cursor()
-        cursor.execute(DB_INIT_COMMAND)
-
-
-    def Create(self, table, column, newValue):
-        ''' Insert new record into the database '''
-        pass
+        self.openConnection()
+        self.cursor.execute(DB_INIT_COMMAND)
+        self.closeConnection()
+        
+    def openConnection(self):
+        ''' Open database connection '''
+        self.connection = sqlite3.connect(DB)
+        self.cursor = self.connection.cursor()
     
+    def closeConnection(self):
+        ''' Save and close database connection '''
+        self.connection.commit()
+        self.connection.close()
+
+
+    def Create(self, table, column, value):
+        ''' Insert new record into the database '''
+        self.openConnection()
+        query = f"""
+        INSERT INTO {table} ({column})
+        VALUES (?)"""
+
+        self.cursor.execute(query, (value,))
+        self.closeConnection()
+        
+
     def Read(self, table, column):
         ''' Retrieve existing record from the database '''
-        pass
+        self.openConnection()
+
+        self.closeConnection()
     
+
     def Update(self, table, column, newValue):
         ''' Overwrite an existing record in the database '''
-        pass
+        self.openConnection()
 
-    def Delete(self):
+        self.closeConnection()
+
+
+    def Delete(self, table, column, existingValue):
         ''' Delete a record from the database '''
-        pass
+        self.openConnection()
+
+        self.closeConnection()
+
+
+    def __del__(self):
+        self.closeConnection()
 
     
 
