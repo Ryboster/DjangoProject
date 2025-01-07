1347909e0d6aaf2fcfc4d2a7a1cdc67519c3c551
23017998 <23017998@uhi.ac.uk>
2025-01-07 10:41:42 +0000

    Added drop-down menu. Added folding to sidebar

--- server/app1/static/app1/content/footer.js
+++
@@ -1,8 +0,0 @@
-function CreateFooter() {
-                return(
-                    <div style={{textAlign: "right", paddingRight: "50px"}}>
-                        2023
-                        <a href="https://github.com/Ryboster/reactJS">Source</a>
-                    </div>
-                )
-            }
